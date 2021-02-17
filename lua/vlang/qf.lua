594422810483dab77909b3a40295ddb92be63158
Tami <tamibam@yahoo.com>
2021-02-17 20:52:49 +0300

    logger to cach cases where qf errors happen

--- lua/vlang/qf.lua
+++ lua/vlang/qf.lua
@@ -39,15 +39,36 @@ local format_issue = function(lines)
   return get_preview(items, lines)
 end
 
+-- Things that skiped the qf and returned empty qf
+local bs = {
+  "Cannot save output binary in a .v file.",
+  "redefinition of function `C.dup2`",
+  "/path/vlangplay/gh_notify/gh_notify.v:158: error: Looks like you are using spaces for indentation.",
+  "cgen error: could not generate string method void_str for type 'void'",
+  "/gh/daemon/main.v:26: warning: A function name is missing from the documentation of pub fn daemonize(pidpath string, run fn ())",
+  "/v/vlib/builtin/cfns.c.v:410:1: conflicting declaration: fn C.dup2(oldfd int, newfd int) int",
+  "/tmp/v/main.vc.12284744985678515411.tmp.c: In function ‘main__Daemon_stop’:",
+  "/tmp/v/main.vc.12284744985678515411.tmp.c:11678:3: error: too few arguments to function ‘kill’"
+}
+
 local get_issues = function(lines)
   local issues = {}
+  local matcher = "^[^.]+.v:%d+:%d+:%s.*"
   local i = 0
   for _, line in ipairs(lines) do
-    if line:match("^[^.]+.v:%d+:%d:%s.*") ~= nil then
+    if line:match(matcher) ~= nil then
       i = i + 1
       issues[i] = { line }
     else
-      table.insert(issues[i], line)
+      if issues[i] == nil then
+        vim.fn.writefile(vim.tbl_flatten({
+          "Content:", vim.tbl_flatten(lines)
+        }), "/tmp/vlang_errors.log", "a")
+        print("issues[i] is nil")
+      end
+      if issues[i] then
+        table.insert(issues[i], line)
+      end
     end
   end
 
@@ -62,6 +83,7 @@ end
 qf.open = function(lines, winnr)
   vim.fn.setqflist({}, "r")
   local issues = get_issues(lines)
+  if type(issues) == "number" then return end
   for _, issue in pairs(issues) do
     vim.fn.setqflist(issue, "a")
   end
